dc3a6a55f7cd555b8767c6af39869597c40b60f7
createjin <createsejin@gmail.com>
2024-02-15 01:53:32 +0900

    2024-02-15-001

--- src/commander.rs
+++ src/commander.rs
@@ -2,6 +2,7 @@ use crate::process::{read_file, output_of_vec, replace_vec};
 use std::io::{self, Write};
 use std::fs::File;
 use std::path::Path;
+
 pub fn save_file(input_path: &str, lines: &Vec<String>) -> io::Result<()> {
     let path = Path::new(input_path);
     let directory = path.parent().unwrap();
@@ -17,25 +18,31 @@ pub fn save_file(input_path: &str, lines: &Vec<String>) -> io::Result<()> {
 
     Ok(())
 }
-fn open_file() -> io::Result<(Vec<String>, String)> {
-    // TODO: 이 부분에서 상대 경로 path에 대해서도 작동하게 만들어야함.
-    print!("input file path > ");
+fn get_input(prompt: &str) -> String {
+    print!("{}", prompt);
     io::stdout().flush().unwrap();
 
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
+    input.trim().to_string()
+}
 
-    let input = input.trim(); // Remove leading and trailing whitespaces
-    if input.to_lowercase().as_str() == "q" {
+fn get_file_path() -> io::Result<String> {    
+    let input = get_input("file path > ");
+
+    if input == "q" {
         println!("You don't want to open a file.\nProgram will be terminated.");
         std::process::exit(0);
     }
+    // 콤마가 입력된 경우 콤마를 제거하기 위해 입력된 String 양 쪽을 얻는다.
     let first_char = input.chars().next();
     let last_char = input.chars().last();
 
-    let file_path = if first_char == Some('\"') && last_char == Some('\"') {
+    let file_path = 
+    if first_char == Some('\"') && last_char == Some('\"') || 
+    first_char == Some('\'') && last_char == Some('\'') {
         // If the input is surrounded by quotes, remove them
         format!(r"{}", &input[1..input.len()-1])
     } else {
@@ -50,68 +57,96 @@ fn open_file() -> io::Result<(Vec<String>, String)> {
     // 함수의 결과를 String 타입으로 변환한다.
     // to_string_lossy() 함수는 유효하지 않은 유니코드 문자가 있을때에는 �로 변환하고 Owned 타입을 반환한다.
     // 유효한 유니코드 문자가 있을때에는 Borrowed 타입을 반환한다.
-    let lines = read_file(&absolute_path.to_string_lossy().to_string())?;
+    Ok(absolute_path.to_string_lossy().to_string())
+}
 
+fn open_and_read_file() -> io::Result<(Vec<String>, String)> {
+    let file_path = get_file_path()?;    
+    let lines = read_file(&file_path)?;
     Ok((lines, file_path))
 }
-pub fn commander() {
-    crate::process::set_debug(false);
-    loop {
-        #[allow(unused_assignments)]
-        let mut is_success_replace: bool = false;
-        let debug = crate::process::get_debug();
-        match open_file() {
-            Ok((mut lines, file_path)) => {
-                println!("Successfully opened file. \u{25A0}");
-                // Do something with lines
-                if debug {
-                    output_of_vec(&lines, 150);
-                    println!("------------------------------------------------------------");
+
+fn reformat_process() {
+    #[allow(unused_assignments)]
+    let mut is_success_replace: bool = false;
+    let debug = crate::process::get_debug();
+    match open_and_read_file() {
+        Ok((mut lines, file_path)) => {
+            println!("Successfully opened file. \u{25A0}");
+            // Do something with lines
+            if debug {
+                output_of_vec(&lines, 150);
+                println!("------------------------------------------------------------");
+            }
+            let input = 
+                get_input("Do you want to make a reformatted file? (y/n) > ");            
+            match input.trim().to_lowercase().as_str() {
+                "y" => {
+                    replace_vec(&mut lines);
+                    is_success_replace = true;
+                    if debug {
+                        println!("output of lines:");
+                        output_of_vec(&lines, 200);
+                        println!("------------------------------------------------------------");
+                    }
+                },
+                _ => {
+                    println!("You don't want to make a reformatted file.");
                 }
-                print!("Do you want to make a reformatted file? (y/n) > ");
-                io::stdout().flush().unwrap();
-                let mut input = String::new();
-                io::stdin()
-                    .read_line(&mut input)
-                    .expect("Failed to read line");
+            }
+            if is_success_replace {
+                let input = 
+                    get_input("Do you want to save the reformatted file? (y/n) > ");
                 match input.trim().to_lowercase().as_str() {
                     "y" => {
-                        replace_vec(&mut lines);
-                        is_success_replace = true;
-                        if debug {
-                            println!("output of lines:");
-                            output_of_vec(&lines, 200);
-                            println!("------------------------------------------------------------");
+                        match save_file(&file_path, &lines) {
+                            Ok(_) => println!("Successfully saved the reformatted file. \u{25A0}"),
+                            Err(e) => println!("Failed to save the reformatted file: {} \u{25A1}", e),
                         }
                     },
                     _ => {
-                        println!("You don't want to make a reformatted file.");
+                        println!("You don't want to save the reformatted file.");
                     }
                 }
-                if is_success_replace {
-                    print!("Do you want to save the reformatted file? (y/n) > ");
-                    io::stdout().flush().unwrap();
-                    let mut input = String::new();
-                    io::stdin()
-                        .read_line(&mut input)
-                        .expect("Failed to read line");
-                    match input.trim().to_lowercase().as_str() {
-                        "y" => {
-                            match save_file(&file_path, &lines) {
-                                Ok(_) => println!("Successfully saved the reformatted file. \u{25A0}"),
-                                Err(e) => println!("Failed to save the reformatted file: {} \u{25A1}", e),
-                            }
-                        },
-                        _ => {
-                            println!("You don't want to save the reformatted file.");
-                        }
-                    }
-                }
-            },
-            Err(e) => {
-                println!("Failed to open file: {} \u{25A1}", e);
-                // Continue the loop to prompt the user again
             }
+        },
+        Err(e) => {
+            println!("Failed to open file: {} \u{25A1}", e);
+            // Continue the loop to prompt the user again
+        }
+    }
+}
+
+fn fix_sub_process() {
+    let debug = crate::process::get_debug();
+    match open_and_read_file() {
+        Ok((lines, file_path)) => {
+            if debug {
+                println!("Successfully opened file. \u{25A0}");
+                println!("file = {}", file_path);
+                for line in &lines {
+                    println!("{}", line);
+                }
+            }            
+        },
+        Err(e) => {
+            println!("Failed to open file: {} \u{25A1}", e);
+        }
+    }
+}
+
+pub fn commander() {
+    crate::process::set_debug(false);
+    loop {
+        let command = get_input("cmd> ");
+        match command.as_str() {
+            "reformat sub" => reformat_process(),
+            "fix sub" => fix_sub_process(),
+            "exit" => {
+                println!("exit program");
+                break;
+            }
+            _ => (),
         }
     }
 }
